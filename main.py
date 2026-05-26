fdd1777c03fa950f409805f13572e179bd7ae450
Zukach1201 <sm.poveda@uniandes.edu.co>
2026-05-26 18:01:46 -0500

    Este es el commit inicial: resenadestacada funciona NO LA ROMPAN

--- main.py
+++ main.py
@@ -14,44 +14,36 @@ app.add_middleware(
 )
 
 
-client = MongoClient(os.environ["MONGO_URI"])
-db = client[os.environ["MONGO_DB"]]
+##client = MongoClient(os.environ["MONGO_URI"])
+client = MongoClient("mongodb://ISIS2304B19202610:fgMUV1ChCzyg@157.253.236.88:8087/")
+db = client["ISIS2304B19202610"]
 
 
 @app.get("/")
 def inicio():
-    return {"estado": "API funcionando correctamente...? JAJJA"}
+    return {"estado": "API funcionando correctamente..."}
 
-@app.get("/bares")
-def get_bares():
-    return list(db["Bares"].find({},{'_id':0}))
+@app.get('/hoteles/{hotel_id}/resenadestacada')
+def get_resenadestacada(hotel_id: str):
+    # Buscamos las reseñas del hotel, ordenamos por votosUtiles de mayor a menor (-1) y traemos la primera (.limit(1))
+    resena_mas_votada = list(db["resenas"].find(
+        {'hotelId': hotel_id, 'estado': 'PUBLICADA'},
+        {'_id': 0}
+    ).sort('votosUtiles', -1).limit(1))
 
-@app.post("/bares")
-def post_bares(datos: list = Body(...)):
-    resultado = db["Bares"].insert_many(datos)
-    return {"resultado": "UwU"}
+    return resena_mas_votada[0] if resena_mas_votada else None
 
-@app.get('/bares/{bar_id}/comentarios')
-def get_comentarios(bar_id: int):
-    comentarios = db["Bares"].find_one({'_id': bar_id},{'comentarios':1,"_id":0})
-    return comentarios['comentarios'] if comentarios and 'comentarios' in comentarios else []
+@app.get('/hoteles/{hotel_id}/resenas')
+def get_resenas(hotel_id: str):
+    resenas = db["resenas"].find({'hotelId': hotel_id},{"usuarioId":1,"fecha":1,"calificacion":1,"comentario":1,"votosUtiles":1,"_id":0})
+    return list(resenas) if resenas else []
 
-@app.post('/bares/{bar_id}/comentarios')
-def post_comentario(bar_id: int, datos: dict):
-    # datos['bar_id'] = bar_id // No es necesario agregar bar_id al comentario, ya que se almacenará dentro del documento del bar correspondiente
+@app.post('/hoteles/{hotel_id}/resenas')
+def post_resena(hotel_id: str, datos: dict):
+    datos['hotelId'] = hotel_id
     datos['fecha']  = datetime.now().isoformat()
-    db["Bares"].update_one({'_id': bar_id}, {'$push': {'comentarios': datos}})
-    return {'mensaje': 'Comentario guardado'}
-
-@app.get('/bares/{bar_id}/eventos')
-def get_eventos(bar_id: int):
-    eventos = db["Bares"].find_one({'_id': bar_id}, {'eventos': 1,"_id": 0})
-    return eventos['eventos'] if eventos and 'eventos' in eventos else []
-
-@app.post('/bares/{bar_id}/eventos')
-def post_evento(bar_id: int, datos: dict): 
-    # datos['bar_id'] = bar_id // No es necesario agregar bar_id al evento, ya que se almacenará dentro del documento del bar correspondiente
-    datos['fecha_creacion']  = datetime.now().isoformat()
-    db["Bares"].update_one({'_id': bar_id}, {'$push': {'eventos': datos}})
-    return {'mensaje': 'Evento guardado'}
+    datos['estado'] = 'PUBLICADA' ##confiamos que se publicó correctamente, pero se podría agregar un campo de estado para manejar casos de moderación o errores
+    datos['votosUtiles'] = 0
+    db["resenas"].insert_one(datos)
+    return {'mensaje': 'Reseña guardada correctamente'}
 
