de772f6464871c5678a36b6a4e7342e6f067a52a
Zukach1201 <sm.poveda@uniandes.edu.co>
2026-05-26 18:36:34 -0500

    shhhh

--- main.py
+++ main.py
@@ -14,10 +14,8 @@ app.add_middleware(
 )
 
 
-##client = MongoClient(os.environ["MONGO_URI"])
-client = MongoClient("mongodb://ISIS2304B19202610:fgMUV1ChCzyg@157.253.236.88:8087/")
-db = client["ISIS2304B19202610"]
-
+client = MongoClient(os.environ["MONGO_URI"])
+db = client[os.environ["MONGO_DB"]]
 
 @app.get("/")
 def inicio():
